74c08bc9fcc160e7c036b28f49d9017a79ce68d0
Arnaud Delcasse <arnaud@delcasse.fr>
2013-12-24 11:25:19 +0100

    Implement carousel and social icons with the socicons font

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8f6062b3e019c9730cca28f311a531f2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '00fe5a5030be7f9bb60675347a2d3f62');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
---
+++ templates/content-frontpage.php
@@ -0,0 +1,42 @@
+<div id="carousel-frontpage" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-frontpage" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-frontpage" data-slide-to="1"></li>
+    <li data-target="#carousel-frontpage" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="http://gaucheunie-antibes.fr/carte_voeux_recto.png" alt="La gauche unie" class="aligncenter">
+      <div class="carousel-caption">
+        <h3>Inauguration du local de campagne</h3>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://gaucheunie-antibes.fr/carte_voeux_verso.png" alt="La gauche unie" class="aligncenter">
+      <div class="carousel-caption">
+        <h3>Inauguration du local de campagne</h3>
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-frontpage" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-frontpage" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+
+
+<section class="share">
+  <div class="container">
+    <ul class="list-share">
+      <li><a class="btn" href="https://twitter.com/GaucheUnie"><i class="icon-twitter"></i> Twitter</a></li>
+      <li><a class="btn" href="https://facebook.com/GaucheUnie"><i class="icon-facebook"></i> Facebook</a></li>
+    </ul>
+  </div>
+</section>
