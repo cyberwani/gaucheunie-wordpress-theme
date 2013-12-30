237abfbb3d1f880eaf4c188fbbef21f206d5182e
Arnaud Delcasse <arnaud@delcasse.fr>
2013-12-30 14:06:04 +0100

    Many changes in layout of the front page

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e9e4b36571ec00cbce819e5323a2748e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '40078ca19ec5ab5debd1ea49d9ff54f6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/content-frontpage.php
+++ templates/content-frontpage.php
@@ -1,12 +1,12 @@
 <div id="header-frontpage">
   <div class="container row-fluid aligncenter">
-    <div  itemscope itemtype="http://data-vocabulary.org/Organization" id="presentation" class="col-md-8">
-      <p><span class="name">La gauche unie, solidaire et écologique</span> est une <span itemprop="description">liste de rassemblement de la gauche pour les élections municipales 2014 à Antibes-Juan les Pins, menée par <a href="/michele-muratore/">Michèle Muratore</span>.</p>
-      <p></p>
+    <div itemscope itemtype="http://data-vocabulary.org/Organization" id="presentation" class="presentation col-md-8">
+      <img src="/wp-content/themes/gaucheunieantibes/assets/img/gauche_unie_banner.png" alt="La gauche unie, solidaire et écologique - Michèle Muratore pour Antibes Juan les Pins">
+      <h2>La gauche unie, solidaire et écologique - Michèle Muratore pour Antibes Juan les Pins</h2>
+      <p>Les élections municipales se dérouleront les 23 et 30 Mars 2014. <a href="/"><em><span itemprop="name">La gauche unie, solidaire et écologique</span></em></a> est une <span itemprop="description"><strong>liste de rassemblement à gauche pour Antibes Juan les Pins</strong> menée par <strong><span itemprop="founder" itemscope itemtype="http://data-vocabulary.org/Person">Michèle Muratore</span></strong></span>.</p>
     </div>
     <div class="col-md-4">
     <div id="social-icons">
-      <h3>Nous suivre</h3>
       <p>
         <a href="https://twitter.com/GaucheUnie"><img src="/wp-content/themes/gaucheunieantibes/assets/img/twitter.png"></a>
         <a href="https://facebook.com/GaucheUnie"><img src="/wp-content/themes/gaucheunieantibes/assets/img/facebook.png"></a>
@@ -18,55 +18,31 @@
     <div class="header-title header-title-calendar">
     <a href="/calendar/">
       <p>
-        <img src="/wp-content/themes/gaucheunieantibes/assets/img/calendar3.png">
+        <img src="/wp-content/themes/gaucheunieantibes/assets/img/calendar3.png" alt="Calendrier de campagne">
       </p>
       </a>
     </div>
 
-    <div class="header-title header-title-newsletter">
+     <div class="header-title header-title-newsletter">
+      <a href="/questionnaire/">
+      <p>
+        <img src="/wp-content/themes/gaucheunieantibes/assets/img/donner_avis2.png" alt="Donner votre avis">
+      </p>
+      </a>
+    </div>
+
+   <div class="header-title header-title-newsletter">
       <a href="/etre-informe/">
       <p>
-        <img src="/wp-content/themes/gaucheunieantibes/assets/img/inscription_newsletter2.png">
+        <img src="/wp-content/themes/gaucheunieantibes/assets/img/inscription_newsletter2.png" alt="Etre informé / Newsletter">
       </p>
       </a>
     </div>
 
-    <div class="header-title col-md-6">
-      <a href="/questionnaire-donnez-nous-votre-avis/">
-      <h3>Donner votre<br>avis</h3>
-      <p>
-        <img src="/wp-content/themes/gaucheunieantibes/assets/img/donner_avis.png">
-      </p>
-      </a>
-    </div>
-
-    <!--<div id="carousel-frontpage" class="carousel slide col-md-8" data-ride="carousel">
-
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-frontpage" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-frontpage" data-slide-to="1"></li>
-        <li data-target="#carousel-frontpage" data-slide-to="2"></li>
-      </ol>
-
-      <div class="carousel-inner">
-        <?php while (have_posts()) : the_post(); ?>
-          <?php get_template_part('templates/content', 'carouselitem'); ?>
-        <?php endwhile; ?>
-      </div>
-
-      <a class="left carousel-control" href="#carousel-frontpage" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-      </a>
-      <a class="right carousel-control" href="#carousel-frontpage" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-      </a>
-    </div>-->
-
-    <div class="header-title col-md-6">
+    <div class="header-title header-title-contact">
       <a href="/nous-rencontrer-nous-contacter">
-      <h3>Nous rencontrer / contacter</h3>
       <p>
-        <img src="/wp-content/themes/gaucheunieantibes/assets/img/preview_osm_local.png" class="map">
+        <img src="/wp-content/themes/gaucheunieantibes/assets/img/map_contact.png" alt="Nous rencontrer / Nous contacter">
       </p>
       </a>
     </div>
